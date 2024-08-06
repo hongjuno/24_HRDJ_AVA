d248b6e04f8ab46963e3393972778c912feb1e19
hongjuno <wnsdh2397@gmail.com>
2024-08-06 11:31:25 +0900

    Add files via upload
    첫번째 예제

---
+++ Hello.java
@@ -0,0 +1,20 @@
+package day01;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*여러줄 주석*/
+		/**
+		 * 
+		 * 
+		 */
+		System.out.print("Hello Java\nHello Java\nHello Java\n");
+		/*
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		System.out.println("Hello Java");
+		*/
+	}
+
+}
